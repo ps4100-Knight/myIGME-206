8c67f4bf883d7294bf8834030c0ca707625afa61
ps4100 <ps4100@g.rit.edu>
2021-08-24 22:41:37 -0400

    started fav color and num app

---
+++ FavoriteColourAndNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColourAndNumber
+{
+    // Class: Program
+    // Authore: Pruthviraj Solanki
+    // Purpose: Console Read/Write and Exception-handling exercise
+    // Restrictions: none
+    class Program
+    {
+        // Method: Main
+        // Purpose: Prompt the user for their favorite color and number
+        //          Output their favourite color (in limited text colors) Their favorite number of times
+        // Restrictions: none
+        static void Main(string[] args)
+        {
+            // string to hold their favorite color
+            string color = null;
+
+            // int to hold thier favorite number
+            int favnum = 0;
+
+            // flag to indicae if they entered a valid number string
+            bool bValid = false;
+
+            // loop counter
+            int i = 0;
+
+            Console.Write("Enter your favorite color:\t");
+
+            color = Console.ReadLine();
+        }
+    }
+}
