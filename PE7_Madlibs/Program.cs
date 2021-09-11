c41a710852730b025c36480bb91461ed195ba723
ps4100 <ps4100@g.rit.edu>
2021-09-11 17:32:07 -0400

    a

--- PE7_Madlibs/Program.cs
+++ PE7_Madlibs/Program.cs
@@ -3,13 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace PE7_Madlibs
+namespace PE7__MathLib
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int numLibs = 0;
+            int cntr = 0;
+            int nChoice = 0;
+
+            StreamReader input;
+
+            // open the template file to count how many Mad Libs it contains
+            input = new StreamReader("MadLibsTemplate.txt");
+
+            string line = null;
+            while ((line = input.ReadLine()) != null)
+            {
+                ++numLibs;
+            }
+
+            // close it
+            input.Close();
+            // only allocate as many strings as there are Mad Libs
+            string[] madLibs = new string[numLibs];
+
+            input = new StreamReader("MadLibsTemplate.txt");
+
+            line = null;
+            while ((line = input.ReadLine()) != null)
+            {
+                // set this array element to the current line of the template file
+                madLibs[cntr] = line;
+
+                // replace the "\\n" tag with the newline escape character
+                madLibs[cntr] = madLibs[cntr].Replace("\\n", "\n");
+
+                ++cntr;
+            }
+
+            input.Close();
+
+
+            string[] words = madLibs[nChoice].Split(' ');
+            string result = "";
+            foreach (string word in words)
+            {
+                // if word is a placeholder
+                // prompt the user for the replacement
+                // and append the user response to the result string
+                // else append word to the result string
+                if (word.StartsWith("{"))
+                {
+                    string pword = word;
+                    pword.Replace('{', ' ');
+                    pword.Replace('}', ' ');
+                    Console.WriteLine("Enter a " + pword);
+                    string add = Console.ReadLine();
+                    result += " " + add;
+                }
+                else
+                {
+                    result += " " + word;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
