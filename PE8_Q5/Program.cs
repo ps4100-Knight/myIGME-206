f6d5844bc1535ccc17838763092cfe8e06d9d003
ps4100 <ps4100@g.rit.edu>
2021-09-13 22:11:38 -0400

    PE8_Q5

---
+++ PE8_Q5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8___Q5
+{
+    //Author: Pruthviraj Solanki
+    //Purpose: A code that solves the formula : z = 3y2 + 2x - 1 
+    class Program
+    {
+        //Author : Pruthviraj Solanki
+        //Purpose: Decalres variables to hole values for x,y,z in a 3D double data type array.
+
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+
+            int nX = 0;
+            int nY = 0;
+
+            double[,,] zFun = new double[30, 40, 3];
+
+            // loop through each value of x
+            for (x = -1; x <= 1; x += 0.1, ++nX)
+            {
+                x = Math.Round(x, 1);
+
+                nY = 0;
+
+                // loop through each value of y
+                for (y = 1; y <= 4; y += 0.1, ++nY)
+                {
+                    y = Math.Round(y, 1);
+
+                    z = 3 * Math.Pow(y, 2) + 2 * x + 1;
+
+                    z = Math.Round(z, 3);
+
+                    // store x, y and z for this (x,y) value
+                    zFun[nX, nY, 0] = x;
+                    zFun[nX, nY, 1] = y;
+                    zFun[nX, nY, 2] = z;
+                }
+            }
+        }
+    }
+}
--- PE8_Q7/Program.cs
+++ PE8_Q7/Program.cs
@@ -14,22 +14,9 @@ namespace PE8___Q7
         //Purpose: Reads the input string from user and reverses all the characters 
         static void Main(string[] args)
         {
-            //variables
-            string oString = null;
-            string nString = null;
-            int sLength = 0;
-            // Reading input from user
-            Console.Write("Enter the string: ");
-            oString = Console.ReadLine();
+            string[] blab = new string[5];
+            string[5] = 5th string;
 
-            sLength = oString.Length - 1;
-            //Inverting the string
-            for (int i = sLength; i >= 0; i--)
-            {
-                nString += oString[i];
-            }
-
-            Console.WriteLine("Your reverse order string is:" + nString);
         }
     }
 }
