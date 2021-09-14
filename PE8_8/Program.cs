e254c0bc954b8d934e9b1a1d37913119fdae8ff8
ps4100 <ps4100@g.rit.edu>
2021-09-13 21:39:40 -0400

    PE8 Q8

---
+++ PE8_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uString = null;
+            string rString = null;
+            Console.WriteLine("Enter a string which includes word 'no' in it\n the result string will have 'no' replaced by 'yes'\n");
+            uString = Console.ReadLine();
+            rString = uString.ToLower();
+            
+            Console.WriteLine("The resulting string is :\n " + rString.Replace("no", "yes");
+
+        }
+    }
+}
