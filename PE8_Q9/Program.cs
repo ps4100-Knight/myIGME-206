3f66c0a02df0dd20c1118022920aa2efbd4d7fee
ps4100 <ps4100@g.rit.edu>
2021-09-13 21:48:58 -0400

    PE8_Q9

---
+++ PE8_Q9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8__Q9
+{
+    //Author: Pruthviraj Solanki
+    //Purpose: Adds double quoats around all words
+    class Program
+    {
+        //Author: Pruthviraj Solanki
+        //Purpose: Prints all the words with double quoats around them
+        static void Main(string[] args)
+        {
+            string uString;
+            Console.Write("Enter a string: ");
+            uString = Console.ReadLine();
+
+            String[] str = uString.Split(' ');
+
+            foreach (string word in str)
+            {
+                Console.Write("\"" + word + "\"");
+            }
+        }
+    }
+}
