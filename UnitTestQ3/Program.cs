75a882fd0344089ba880fccc8a6aa1b94fe11050
ps4100-Knight <ps4100@g.rit.edu>
2021-09-26 18:18:54 -0400

    UnitTest Q3

---
+++ UnitTestQ3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestQ3
+{ 
+    //Author: Pruthviraj Solanki(Knight)
+    //Purpose: Declares a delegate function that impersonates ReadLine function
+    class Program
+    {
+        delegate string readLineFunctionDelegate();
+
+        //Author : Pruthviraj Solanki(Knight)
+        //Purpose: Uses the delegate that impersonates ReadLine and takes input form user.
+        static void Main(string[] args)
+        {
+            readLineFunctionDelegate Rline = new readLineFunctionDelegate(Console.ReadLine);
+            Console.Write("Please enter a string: ");
+            string newString = Rline();
+            Console.WriteLine("String that you entered was :\n " + newString);
+        }
+    }
+}
