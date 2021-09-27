b299d946d26754bf7172e02d74985f651d3ef5b1
ps4100-Knight <ps4100@g.rit.edu>
2021-09-26 20:28:47 -0400

    UnitTestQ12

---
+++ UnitTestQ12/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+
+namespace UT_12
+{
+
+    // Author :- Pruthviraj Solanki (Knight)
+    /* Purpose :- The function increases the salary by $19,999.99 if name = your name and return true
+                  Otherwise it returns false.
+    The main program congratulates the user if they got a raise, and displays their new salary.*/
+
+    class Program
+    {
+        // Author :- Pruthviraj Solanki (Knight)
+        // Purpose :- A Console application for salary raise for a specific name
+
+        static bool GiveRaise(string name, ref double salary)
+        // Function to increase Salary
+        {
+            if (name.ToLower () == "pruthviraj")
+            {
+                salary = salary + 19999.99;  // if condition = true increase salary
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("Enter Your Name");
+            sName = Console.ReadLine();
+
+            if (GiveRaise(sName, ref dSalary) == true)   // checking bool condition, if its true increase salary
+            {
+                Console.WriteLine("Congratulations, you got salary increase");
+                Console.WriteLine("This is your New Salary " + dSalary);
+            }
+            else // checking bool condition, if its false default salary
+            {
+                Console.WriteLine("This is your  Salary " + dSalary);
+            }
+
+        }
+    }
+}
+
---
+++ UnitTestQ14/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1_BugSquash
+{
+    // Author :- RAJ BAROT
+    // Purpose :- To remove errors and calculate x^y using a recursive function     
+    class Program
+    {
+        // Calculate x^y for y > 0 using a recursive function
+        static void Main(string[] args)
+        {
+            string sNumber;
+            int nX;
+            // int nY    Syntax error no ; <--------
+            int nY;
+
+            int nAnswer;
+
+            //Console.WriteLine(This program calculates x ^ y.);   <--------
+            // Syntax Error no "" in writelinefunction
+            Console.WriteLine("This program calculates x ^ y");
+
+            do
+            {
+                Console.Write("Enter a whole number for x: ");
+                //Console.ReadLine(); <--------
+                // we need to initialize sNumber so we add the input from console readline into sNumber
+                // Syntax Error
+                sNumber = Console.ReadLine();
+
+            } while (!int.TryParse(sNumber, out nX));
+
+
+            do
+            {
+                Console.Write("Enter a positive whole number for y: ");
+                sNumber = Console.ReadLine();
+            }
+            while (!int.TryParse(sNumber, out nY));
+            //while (int.TryParse(sNumber, out nX)); <--------
+            // we took the input of y into nX which makes no sense  and we didnt had "!int.TryParse" but "int.TryParse" which takes us in infinite loop of input
+            //Logical Error
+
+
+            // compute the factorial of the number using a recursive function
+
+            nAnswer = Power(nX, nY);
+
+
+            // Console.WriteLine("{nX}^{nY} = {nAnswer}");  <--------
+            // That line does not print values of x and y but just the text
+            Console.WriteLine("X^Y = " + nAnswer);
+        }
+
+
+        static int Power(int nBase, int nExponent)
+        // static keyword was missing in the function 
+        {
+            int returnVal = 0;  
+            int nextVal = 0;    
+            // We dont need these Variables as well
+
+            // the base case for exponents is 0 (x^0 = 1)
+
+            //WE CAN DO ALL THE EENTIRE FUNCTION IN 2 LINES OF CODE
+
+         /*  if (nExponent != 0)
+            {
+                return (nBase * Power(nBase, nExponent - 1));
+            }
+            return 1;*/
+
+            if (nExponent == 0)
+            {
+                // return the base case and do not recurse
+                returnVal = 0;
+            }
+            else
+            {
+                // compute the subsequent values using nExponent-1 to eventually reach the base case
+                nextVal = Power(nBase, nExponent + 1);
+
+                // multiply the base with all subsequent values
+                returnVal = nBase * nextVal;
+            }
+
+
+            //returnVal;  <--------
+            // need a return a value since it is int type function 
+            // SYntax Error
+            return  returnVal;
+
+        }
+    }
+}
+
