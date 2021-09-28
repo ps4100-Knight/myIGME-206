2b040ae19895278cc271058c35d67a0d0d924d3b
ps4100-Knight <ps4100@g.rit.edu>
2021-09-27 23:36:48 -0400

    PE11Q6

---
+++ PE11Q6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddPassenger(new Class1.Compact());
+            AddPassenger(new Class1.SUV());
+      
+        }
+        static void AddPassenger(Class1.IPassengerCarrier Vehicle)
+        {
+            
+            Console.WriteLine(Vehicle.ToString());
+        }
+    }
+}
--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    public class Class1
+    public abstract class Class1
     {
         public abstract class Vehicle
         {
