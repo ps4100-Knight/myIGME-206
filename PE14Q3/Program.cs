b22f06a5aabd3415d64eae71c7b4facad18c6556
ps4100-Knight <ps4100@g.rit.edu>
2021-10-06 21:45:57 -0400

    PE14DLLANDONSOLE

---
+++ PE14Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PECLASSLIB;
+
+namespace PE14Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Obj = new Class1();
+            Class2 Class2Obj = new Class2();
+            MyMethod(class1Obj);
+            MyMethod(Class2Obj);
+        }
+        public static void MyMethod(Interface1 myObject)
+        {
+
+            myObject.method();
+        }
+    }
+}
---
+++ PECLASSLIB/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PECLASSLIB
+{
+    public interface Interface1
+    {
+        public void method();
+       
+    }
+    public class Class1 : Interface1
+    {
+        public void method()
+        {
+            Console.WriteLine("Method inside Class 1\n");
+        }
+    }
+    public class Class2 : Interface1
+    {
+        public void method()
+        {
+            Console.WriteLine("Method inside Class 1\n");
+        }
+    }
+}
