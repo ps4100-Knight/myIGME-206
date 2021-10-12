a35990aaf564633b4159996d48470bec4cc5f5cb
ps4100-Knight <ps4100@g.rit.edu>
2021-10-11 23:10:36 -0400

    asda

---
+++ pe16/Class1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe16
+{
+    // Author : Pruthviraj (Knight)
+    // Purpose : Changing UML diagram to class lib
+    public interface IMood
+    {
+        string Mood
+        {
+            get;
+        }
+    }
+    public interface ITakeOrder
+    {
+        void TakeOrder();
+    }
+
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        private byte sugar;
+        public string size;
+        public Customer customer;
+
+        public virtual void AddSugar(byte amount)
+        {
+
+        }
+
+        public abstract void Steam();
+
+        public HotDrink()
+        {
+
+        }
+
+        public HotDrink(string brand)
+        {
+
+        }
+    }
+
+    public class Waiter : IMood
+    {
+        public string name;
+        public string Mood
+        {
+            get => name;
+        }
+        public void ServeCustomer(HotDrink cup)
+        {
+
+        }
+    }
+
+    public class Customer : IMood
+    {
+        public string name;
+        public string creditCardNumber;
+        public string Mood
+        {
+            get => name;
+        }
+    }
+
+    
+   
+    public class CupOfCoffee : HotDrink, ITakeOrder
+    {
+        public string beanType;
+        public override void Steam()
+        {
+
+        }
+        public void TakeOrder()
+        {
+
+        }
+        public CupOfCoffee(string brand) : base(brand)
+        {
+
+        }
+    }
+
+    public class CupOfTea : HotDrink, ITakeOrder
+    {
+        public string leafType;
+        public override void Steam()
+        {
+
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+
+        public CupOfTea(bool customerIsWealthy)
+        {
+
+        }
+    }
+
+    public class CupOfCocoa : HotDrink, ITakeOrder
+    {
+        public static int numCups;
+        public bool marshmallows;
+        private string source;
+
+        public string Source
+        {
+            set => source = value;
+        }
+
+        public override void Steam()
+        {
+
+        }
+
+        public override void AddSugar(byte amount)
+        {
+
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+
+        public CupOfCocoa() : this(false)
+        {
+
+        }
+
+        public CupOfCocoa(bool marshmallows) : base("Expensive Organic Brand")
+        {
+
+        }
+    }
+
+
+
+
+}
