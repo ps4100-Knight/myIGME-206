eea4b49009981a66f589f833957c50e29b506767
ps4100-Knight <ps4100@g.rit.edu>
2021-10-21 19:59:37 -0400

    asda

---
+++ UT2Q3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT2Q3
+{
+    //Author :- Pruthviaj Solanki (Knight)
+    //Purpose 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 0;
+            double w = -2;
+            SortedList<(double, double, double), double> sortedlist = new SortedList<(double, double, double), double>();
+            //sortedlist[(0, 0, 0)] = 5;
+            //Console.WriteLine(sortedlist[(0,0,0)]);
+            for (w = -2; w<=0; Math.Round(w+=0.2,1))
+            {
+                for (y = -1; y <= 1; Math.Round(y += 0.1, 1))
+                {
+                    for (x = 0; x<=4; Math.Round(x += 0.1,1))
+                    {
+                        sortedlist[(w,x,y)] = Math.Round(( (4* Math.Pow(y,3)) + (2* Math.Pow(x,2)) - (8*w) + 7 ),3);
+              //          Console.WriteLine(sortedlist[(w, x, y)]);
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ UT2Q4/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT2Q4
+{
+    
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber;
+
+        public abstract void Connect();
+
+        public abstract void Disconnect();
+    }
+
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get;
+        }
+
+        public string FemaleSideKick
+        {
+            get;
+        }
+
+        public void TimeTravel()
+        {
+
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneCall;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+}
